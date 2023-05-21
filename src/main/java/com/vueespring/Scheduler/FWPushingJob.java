75603142b7a3d47225cca9a95635c2821b8b4a8c
Arc <13930228843@163.com>
2023-05-21 10:43:50 +0800

    最后一次提醒消除混淆

--- src/main/java/com/vueespring/Scheduler/FWPushingJob.java
+++ src/main/java/com/vueespring/Scheduler/FWPushingJob.java
@@ -2,18 +2,17 @@ package com.vueespring.Scheduler;
 import com.vueespring.service.FWAlertJobService;
 import com.vueespring.service.serviceImpl.FWAlertJobServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.jni.Time;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 @Slf4j
 public class FWPushingJob extends QuartzJobBean {
     FWAlertJobService fwAlertJobService = new FWAlertJobServiceImpl();
-    /**
-     * @param jobExecutionContext
-     */
     @Override
     protected void executeInternal(org.quartz.JobExecutionContext jobExecutionContext) {
         JobDetail detail = jobExecutionContext.getJobDetail();
@@ -21,6 +20,10 @@ public class FWPushingJob extends QuartzJobBean {
         Map<String,Object> map = new HashMap<>();
         log.debug(String.valueOf(jobDataMap));
         fwAlertJobService.setMapByJob(jobDataMap, map);
+        if(LocalDateTime.now().isAfter((LocalDateTime)map.get("end_time"))){
+            String message = "Last Mention: " + map.get("message");
+            map.put("message",message);
+        }
         if(map.get("alertToken")==null){
             log.error("token empty!");
         }
